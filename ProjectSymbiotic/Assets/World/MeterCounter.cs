69568df49e09b7ca6186b3433f1fd3011d59c290
SyaibaSetrynn <hoshiya_setrynn@qq.com>
2024-10-06 16:36:52 -0500

    Upupup
    Now the canvas shows the coming up

--- ProjectSymbiotic/Assets/World/MeterCounter.cs
+++ ProjectSymbiotic/Assets/World/MeterCounter.cs
@@ -7,13 +7,21 @@ public class MeterCounter : MonoBehaviour
 {
     // Start is called before the first frame update
     static TMP_Text textt;
-    public static void meterchange(int n)
+    static float n;
+    public static void meterchange(float n)
     {
-        textt.text = n.ToString()+"m";
+        textt.text = n.ToString("F2")+"m";
+    }
+    public static void addmeter(float x)
+    {
+        n += x;
+        meterchange(n);
     }
     void Start()
     {
         textt = GetComponent<TMP_Text>();
+        n = 0;
+        meterchange(n);
     }
 
     // Update is called once per frame
