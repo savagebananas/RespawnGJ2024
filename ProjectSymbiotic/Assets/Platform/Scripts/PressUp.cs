5daa047ae7812bb6c1f12f720b69a6449f1dab0e
Bruno Wu <bruno7405@gmail.com>
2024-10-06 17:07:55 -0500

    Parallax (bugged)

--- ProjectSymbiotic/Assets/Platform/Scripts/PressUp.cs
+++ ProjectSymbiotic/Assets/Platform/Scripts/PressUp.cs
@@ -5,25 +5,15 @@ using UnityEngine;
 public class PressUp : MonoBehaviour
 {
     public float velocity = 1;
-    // Start is called before the first frame update
+
     private void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.GetComponent<DialogManager>() != null)
         {
             if (Input.GetKey(KeyCode.E))
             {
-                MeterCounter.addmeter(Time.deltaTime * velocity);
+                MeterCounter.AddHeight(Time.deltaTime * velocity);
             }
         }
-    }
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
