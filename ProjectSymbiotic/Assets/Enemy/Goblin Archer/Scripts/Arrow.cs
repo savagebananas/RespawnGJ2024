440772ff01038699e7010a04ddf108cdf257a26c
Ralphy105 <146513258+Ralphy105@users.noreply.github.com>
2024-10-07 00:43:48 -0500

    Arrow

--- ProjectSymbiotic/Assets/Enemy/Goblin Archer/Scripts/Arrow.cs
+++ ProjectSymbiotic/Assets/Enemy/Goblin Archer/Scripts/Arrow.cs
@@ -1,18 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class Arrow : MonoBehaviour
 {
+    private Rigidbody2D rb;
+    private SpriteRenderer rend;
+    private int direction = 1;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        rend = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (rb.velocity.x < 0)
+        {
+            rend.flipX = true;
+        }
+        transform.rotation = Quaternion.Euler(0, 0, Mathf.Atan2(rb.velocity.y, rb.velocity.x)*Mathf.Rad2Deg);
     }
 }
