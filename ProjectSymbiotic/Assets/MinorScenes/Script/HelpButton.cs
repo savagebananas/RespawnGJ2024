862306838e89c350375c86b6f57b37d1d0bbb53d
SyaibaSetrynn <hoshiya_setrynn@qq.com>
2024-10-06 21:54:16 -0500

    Help button
    Made help button so that the Whole main menu code is all set.

--- ProjectSymbiotic/Assets/MinorScenes/Script/HelpButton.cs
+++ ProjectSymbiotic/Assets/MinorScenes/Script/HelpButton.cs
@@ -8,15 +8,31 @@ public class HelpButton : MonoBehaviour
 {
     // Start is called before the first frame update
     Button button;
+    public GameObject setting, settext,o1;
+    public GameObject b1, b2, b3;
     public void OnButtonClick()
     {
-        //Help
+        setting.SetActive(true);
+        settext.SetActive(true);
+        o1.SetActive(true);
+        setting.GetComponent<Renderer>().material.color = new Color(1, 1, 1, 0.6f);
+        b1.SetActive(false);
+        b2.SetActive(false);
+        b3.SetActive(false);
     }
     void Start()
     {
         button = GetComponent<Button>();
     }
-
+    public void Setback()
+    {
+        setting.SetActive(false);
+        settext.SetActive(false);
+        o1.SetActive(false);
+        b1.SetActive(true);
+        b2.SetActive(true);
+        b3.SetActive(true);
+    }
     // Update is called once per frame
     void Update()
     {
