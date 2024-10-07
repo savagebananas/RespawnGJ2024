98d2303fdc3a2e3fe7a109d77c00b7f953929e43
ccaton64 <Charlescaton64@gmail.com>
2024-10-07 01:05:53 -0500

    Jump fix / jump off head

--- ProjectSymbiotic/Assets/Players/Player1/PlayerMovement.cs
+++ ProjectSymbiotic/Assets/Players/Player1/PlayerMovement.cs
@@ -71,7 +71,7 @@ public class PlayerMovement : MonoBehaviour
 
     private bool IsGrounded()
     {
-        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+        return Physics2D.OverlapCircle(groundCheck.position, .1f);
     }
 
     private void Flip()
