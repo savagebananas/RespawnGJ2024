cdbbff2136584b2f4d54b36d5a01410ee2ebad30
Bruno Wu <bruno7405@gmail.com>
2024-10-08 00:00:22 -0500

    Level Prototype

--- ProjectSymbiotic/Assets/_General/Parallax/Parallax.cs
+++ ProjectSymbiotic/Assets/_General/Parallax/Parallax.cs
@@ -5,18 +5,11 @@ using UnityEngine.UIElements;
 
 public class Parallax : MonoBehaviour
 {
-
-    private float height;
     public float distanceFromCamera;
 
     void Update()
     {
-        //Debug.Log("Score=" + GameManager.GetHeight());
-        transform.position = new Vector2(transform.position.x, -GameManager.GetHeight() * 25 / distanceFromCamera);
-    }
-
-    public void SetHeight(float h)
-    {
-        height = h;
+        // Debug.Log("Score=" + GameManager.GetHeight());
+        transform.position = new Vector2(transform.position.x, -GameManager.GetHeight() / distanceFromCamera);
     }
 }
