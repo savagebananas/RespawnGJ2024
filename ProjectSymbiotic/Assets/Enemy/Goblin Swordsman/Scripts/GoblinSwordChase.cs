d55d6c34135ebad34dd291f08ee9a4471dea13a2
ccaton64 <Charlescaton64@gmail.com>
2024-10-08 23:46:24 -0500

    Goblin states

--- ProjectSymbiotic/Assets/Enemy/Goblin Swordsman/Scripts/GoblinSwordChase.cs
+++ ProjectSymbiotic/Assets/Enemy/Goblin Swordsman/Scripts/GoblinSwordChase.cs
@@ -1,12 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Rendering;
 using UnityEngine;
 
 public class GoblinSwordChase : State
 {
     [SerializeField] private Enemy enemyBase;
     [SerializeField] private GoblinSwordJump goblinJumpState; // state
-    
+
+    private Transform target;
+    [SerializeField] private float speed;
+
     public override void OnStart()
     {
         Debug.Log("Chase Start");
@@ -15,7 +19,13 @@ public class GoblinSwordChase : State
 
     public override void OnUpdate()
     {
+        target = enemyBase.target;
 
+        float distance = Vector2.Distance(transform.position, target.position);
+        if (distance < 0)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        }
     }
 
     public override void OnLateUpdate(){}
