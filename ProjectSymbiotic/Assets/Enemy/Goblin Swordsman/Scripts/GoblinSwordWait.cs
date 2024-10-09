dcdfb2aad767596553fadde5a2c2dbeef4c88743
ccaton64 <Charlescaton64@gmail.com>
2024-10-08 22:32:54 -0500

    final jump fix for now

--- ProjectSymbiotic/Assets/Enemy/Goblin Swordsman/Scripts/GoblinSwordWait.cs
+++ ProjectSymbiotic/Assets/Enemy/Goblin Swordsman/Scripts/GoblinSwordWait.cs
@@ -4,11 +4,21 @@ using UnityEngine;
 
 public class GoblinSwordWait : State
 {
+    [SerializeField] private Enemy enemyBase;
+    [SerializeField] private GoblinSwordJump goblinJumpState; // state
+
+    public override void OnUpdate()
+    {
+        // When player is within distance, start shooting again
+        var height = transform.position.y;
+        var targetHeight = enemyBase.target.position.y;
+        if (height - targetHeight <= 5 && height >= -2)
+        {
+            stateMachine.setNewState(goblinJumpState);
+        }
+    }
+
     public override void OnStart(){}
-
-    public override void OnUpdate(){}
-
     public override void OnLateUpdate(){}
-
     public override void OnExit(){}
 }
