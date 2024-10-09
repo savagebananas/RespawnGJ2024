709b1b8f55cae46e07d64943ed3eeef58fdcf838
SyaibaSetrynn <hoshiya_setrynn@qq.com>
2024-10-08 19:36:57 -0500

    Lens Mover
    Lens Mover ready (add code)

--- ProjectSymbiotic/Assets/Camera/LensMover.cs
+++ ProjectSymbiotic/Assets/Camera/LensMover.cs
@@ -7,14 +7,26 @@ using UnityEngine;
 public class LensMover : MonoBehaviour
 {
     public static float LensSpeed = 1.0f;
+    public static float MoveSpeed = 2.0f;
     private static float tgt;
+    private static Vector3 dest;
     public static void ChangeLens(float target)
     {
         tgt = target;
     }
+    public static void MoveLens(Vector2 target)
+    {
+        dest = target;
+    }
     void Start()
     {
         tgt = 3.31f;
+        dest = transform.position;
+    }
+    private Vector3 Curved(Vector3 init)
+    {
+        float magi = init.magnitude / MoveSpeed;
+        return init / magi;
     }
 
     void Update()
@@ -32,6 +44,10 @@ public class LensMover : MonoBehaviour
             x -= LensSpeed * Time.deltaTime;
             if (x < tgt) x = tgt;
             cinemachineVirtualCamera.m_Lens.OrthographicSize = x;
+        }
+        if (transform.position!=dest)
+        {
+            transform.position += Curved(dest - transform.position);
         }
     }
 }
