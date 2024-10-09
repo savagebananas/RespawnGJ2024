8a7afcde5ddaf9c24e0c2d13f3cb3d2b1e21fb66
ccaton64 <Charlescaton64@gmail.com>
2024-10-08 21:54:06 -0500

    Im straight uo Gobing my knob

--- ProjectSymbiotic/Assets/Enemy/Goblin Swordsman/Scripts/Goblin Sword Stun.cs
+++ ProjectSymbiotic/Assets/Enemy/Goblin Swordsman/Scripts/Goblin Sword Stun.cs
@@ -2,17 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GoblinSwordStun : MonoBehaviour
+public class GoblinSwordStun : State
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public override void OnStart(){}
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public override void OnUpdate(){}
+
+    public override void OnLateUpdate(){}
+
+    public override void OnExit(){}
 }
