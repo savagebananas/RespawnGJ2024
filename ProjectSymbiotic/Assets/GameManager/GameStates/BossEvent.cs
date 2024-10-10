39dc8860715062c2302c51304286fc2ab183eaa1
Anirudh <63116473+AnirudhJ77@users.noreply.github.com>
2024-10-10 06:19:52 -0500

    gm scene

--- ProjectSymbiotic/Assets/GameManager/GameStates/BossEvent.cs
+++ ProjectSymbiotic/Assets/GameManager/GameStates/BossEvent.cs
@@ -2,17 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BossEvent : MonoBehaviour
+public class BossEvent : GameState
 {
+
+
+
+    public override void OnExit()
+    {
+        GameManager.inEvent = false;
+    }
+
+    public override void OnLateUpdate()
+    {
+
+    }
+
+    public override void OnStart()
+    {
+        GameManager.inEvent = true;
+    }
+
+    public override void OnUpdate()
+    {
+    }
+
+    public override bool StateEnd()
+    {
+        return false;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
