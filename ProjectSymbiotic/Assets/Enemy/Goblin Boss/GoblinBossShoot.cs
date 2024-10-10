f13081d98462ac31b5514b6f6c49afba446ecb46
ccaton64 <Charlescaton64@gmail.com>
2024-10-10 01:03:49 -0500

    gob king ai

--- ProjectSymbiotic/Assets/Enemy/Goblin Boss/GoblinBossShoot.cs
+++ ProjectSymbiotic/Assets/Enemy/Goblin Boss/GoblinBossShoot.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Data;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GoblinBossShoot : State
@@ -8,14 +10,43 @@ public class GoblinBossShoot : State
     [SerializeField] private GoblinBossCharge goblinChargeState; // state
     [SerializeField] private GoblinBossStun goblinStunState; // state
     [SerializeField] private GameObject player2;
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private GameObject chainShot;
 
-    public override void OnUpdate(){}
+    public override void OnUpdate()
+    {
+        if(chainShot.GetComponent<ChainShot>().hasReturned)
+        {
+            Rigidbody2D p2rb = player2.GetComponent<Rigidbody2D>();
+            p2rb.constraints = RigidbodyConstraints2D.None;
+            stateMachine.setNewState(goblinChargeState);
+        }
+    }
 
     public override void OnStart()
     {
+        rb.constraints = RigidbodyConstraints2D.FreezeAll;
         enemyBase.canBeHurt = false;
         Debug.Log("Boss is shooting");
+        Shoot();
     }
+
+    private void Shoot()
+    {
+        Instantiate(chainShot, this.transform.position, this.transform.rotation);
+    }
+
     public override void OnLateUpdate(){}
-    public override void OnExit(){}
+    public override void OnExit()
+    {
+        rb.constraints = RigidbodyConstraints2D.None;
+    }
+
+    void OnCollisionEnter2D (Collision2D col)
+    {
+        if(col.gameObject.tag == "Stalag")
+        {
+            stateMachine.setNewState(goblinStunState);
+        }
+    }
 }
